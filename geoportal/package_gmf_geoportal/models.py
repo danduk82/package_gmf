90ab175adc3431ed00de6723fffa91491738f2f2
Andrea Borghi <andrea.borghi@camptocamp.com>
2020-04-09 10:47:23 +0200

    upgrade to master version of 20200408

--- geoportal/package_gmf_geoportal/models.py
+++ geoportal/package_gmf_geoportal/models.py
@@ -6,5 +6,5 @@ from pyramid.i18n import TranslationStringFactory
 
 from c2cgeoportal_commons.models.main import *  # noqa
 
-_ = TranslationStringFactory('package_gmf-server')
+_ = TranslationStringFactory("package_gmf-server")
 LOG = logging.getLogger(__name__)
--- geoportal/setup.py
+++ geoportal/setup.py
@@ -4,24 +4,15 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='package_gmf_geoportal',
-    version='1.0',
-    description='package_gmf, a c2cgeoportal project',
-    author='package_gmf',
-    author_email='info@package_gmf.com',
-    url='https://www.package_gmf.com/',
-    install_requires=[
-        'c2cgeoportal_geoportal',
-        'c2cgeoportal_admin',
-    ],
+    name="package_gmf_geoportal",
+    version="1.0",
+    description="package_gmf, a c2cgeoportal project",
+    author="package_gmf",
+    author_email="info@package_gmf.com",
+    url="https://www.package_gmf.com/",
+    install_requires=["c2cgeoportal_geoportal", "c2cgeoportal_admin",],
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
-    entry_points={
-        'paste.app_factory': [
-            'main = package_gmf_geoportal:main',
-        ],
-        'console_scripts': [
-        ],
-    },
+    entry_points={"paste.app_factory": ["main = package_gmf_geoportal:main",], "console_scripts": [],},
 )
